b0f12c25e21463b704be1487051cf31f506dc540
doleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2020-06-04 13:15:05 -0400

    Added navigation bar into app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <div class="main">
+                <h1>Interactive Mazes!</h1>
+                <Navigation className="navigation" />
+            </div>
+        </div>
+    );
 }
 
 export default App;
