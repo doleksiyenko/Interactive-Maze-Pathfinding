0632d2a70115d19f7b6c5ac6e9d6ad65cf2ba317
doleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2020-06-05 17:37:43 -0400

    Can create one end/start node and clear them

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,27 +8,39 @@ class Board extends Component {
 
         this.state = {
             nodes: [],
+            startExists: false,
+            endExists: false,
         };
 
-        this.changeStatus = this.changeStatus.bind(this);
+        this.changeNodeStatus = this.changeNodeStatus.bind(this);
+        this.setStart = this.setStart.bind(this);
+        this.setEnd = this.setEnd.bind(this);
     }
 
     createEmptyBoard = () => {
-        // ensure that the nodes list is empty (for board clearing)
+        // create an empty board with
+        let numberOfNodes = 400;
+        let elementsPerRow = 20;
         this.setState({
             nodes: [],
         });
 
-        for (let i = 0; i < 400; i++) {
-            let newNode = [0, i % 20, Math.floor(i / 20)];
+        for (let i = 0; i < numberOfNodes; i++) {
+            let newNode = [
+                0,
+                i % elementsPerRow,
+                Math.floor(i / elementsPerRow),
+            ];
+
             this.setState((prevState) => ({
                 nodes: [...prevState.nodes, newNode],
             }));
         }
     };
 
-    changeStatus = (status, position) => {
-        // when a Node is clicked, it will trigger this method (change its status in this.state.nodes)
+    changeNodeStatus = (status, position) => {
+        // when a Node is clicked, it will trigger this method
+        // (change its status in this.state.nodes)
         let updatedNodes = [...this.state.nodes];
         let index = 20 * position[1] + position[0];
         updatedNodes[index][0] = status;
@@ -37,12 +49,36 @@ class Board extends Component {
         });
     };
 
+    setStart = () => {
+        // the node triggers this event when the 's' key is pressed over it
+        // prevents another start from being created
+        this.setState({
+            endExists: true,
+        });
+    };
+
+    setEnd = () => {
+        // the node triggers this event when the 'e' key is pressed over it
+        // prevents another end from being created
+        this.setState({
+            startExists: true,
+        });
+    };
+
     componentDidMount() {
+        // when the page loads, create an empty board
         this.createEmptyBoard();
     }
 
     componentDidUpdate() {
+        // when the clear board button is clicked in the nav, all the settings are reset
         if (this.props.reset === true) {
+            // clear the start and end nodes
+            this.setState({
+                startExists: false,
+                endExists: false,
+            });
+            // switch off the reset
             this.props.changeReset();
         }
     }
@@ -55,8 +91,12 @@ class Board extends Component {
                         key={[element[1], element[2]]}
                         position={[element[1], element[2]]}
                         status={element[0]}
-                        changeStatus={this.changeStatus}
+                        changeStatus={this.changeNodeStatus}
                         reset={this.props.reset}
+                        startExists={this.state.startExists}
+                        endExists={this.state.endExists}
+                        setStart={this.setStart}
+                        setEnd={this.setEnd}
                     ></Node>
                 ))}
             </div>
--- src/components/Node.js
+++ src/components/Node.js
@@ -25,6 +25,38 @@ class Node extends Component {
         }
     };
 
+    createStartEnd = (event) => {
+        // only create the start/end if the node is not already a wall
+        // can only create one start and one ending point on the board
+        if (this.state.status === 0) {
+            if (event.keyCode === 69 && !this.props.startExists) {
+                // create an end node at this postion if 'e' is pressed
+                // and a start doesn't exist
+                this.setState({
+                    status: 3,
+                });
+                this.props.changeStatus(3, this.props.position);
+                this.props.setEnd();
+            } else if (event.keyCode === 83 && !this.props.endExists) {
+                // create a start node at this position if 's' is pressed
+                // and an end doesn't exist
+                console.log("s");
+                this.setState({
+                    status: 2,
+                });
+                this.props.changeStatus(2, this.props.position);
+                this.props.setStart();
+            }
+        }
+    };
+
+    mouseEnter = () => {
+        document.addEventListener("keydown", this.createStartEnd);
+    };
+    mouseLeave = () => {
+        document.removeEventListener("keydown", this.createStartEnd);
+    };
+
     componentDidUpdate() {
         if (this.props.reset === true) {
             this.setState({
@@ -39,6 +71,8 @@ class Node extends Component {
                 className={`status-${this.state.status}`}
                 id={this.state.position}
                 onClick={this.changeWallPassage}
+                onMouseEnter={this.mouseEnter}
+                onMouseLeave={this.mouseLeave}
             ></div>
         );
     }
