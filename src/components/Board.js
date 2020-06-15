b832acae5f8ba9f0dcec3e5375c1521b45ea834d
doleksiyenko <dan.oleksiyenko@gmail.com>
2020-06-15 13:24:47 -0400

    Board now loads with a border wall

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,168 +4,175 @@ import "./component-css/Board.css";
 import { DFS } from "../algorithms/dfs";
 
 class Board extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            nodes: [],
-            startExists: false,
-            endExists: false,
-            startPos: null,
-            endPos: null,
-            elementsPerRow: 20,
-            visited: [],
-            path: [],
-            showAlgorithm: false,
-        };
+    this.state = {
+      nodes: [],
+      startExists: false,
+      endExists: false,
+      startPos: null,
+      endPos: null,
+      elementsPerRow: 20,
+      visited: [],
+      path: [],
+      showAlgorithm: false,
+    };
 
-        this.changeNodeStatus = this.changeNodeStatus.bind(this);
-        this.setStart = this.setStart.bind(this);
-        this.setEnd = this.setEnd.bind(this);
+    this.changeNodeStatus = this.changeNodeStatus.bind(this);
+    this.setStart = this.setStart.bind(this);
+    this.setEnd = this.setEnd.bind(this);
+  }
+
+  createEmptyBoard = () => {
+    // create an empty board with
+    let elementsPerRow = this.state.elementsPerRow;
+    let numberOfNodes = elementsPerRow ** 2;
+    this.setState({
+      nodes: [],
+    });
+
+    for (let i = 0; i < numberOfNodes; i++) {
+      let newNode;
+      if (
+        i % elementsPerRow === 0 ||
+        i % elementsPerRow === elementsPerRow - 1 ||
+        Math.floor(i / elementsPerRow) === 0 ||
+        Math.floor(i / elementsPerRow) === elementsPerRow - 1
+      ) {
+        newNode = [1, i % elementsPerRow, Math.floor(i / elementsPerRow)];
+      } else {
+        newNode = [0, i % elementsPerRow, Math.floor(i / elementsPerRow)];
+      }
+
+      this.setState((prevState) => ({
+        nodes: [...prevState.nodes, newNode],
+      }));
     }
+  };
 
-    createEmptyBoard = () => {
-        // create an empty board with
-        let elementsPerRow = this.state.elementsPerRow;
-        let numberOfNodes = elementsPerRow ** 2;
-        this.setState({
-            nodes: [],
-        });
+  changeNodeStatus = (status, position) => {
+    // when a Node is clicked, it will trigger this method
+    // (change its status in this.state.nodes)
+    let updatedNodes = [...this.state.nodes];
+    let index = this.state.elementsPerRow * position[1] + position[0];
+    updatedNodes[index][0] = status;
+    this.setState({
+      nodes: updatedNodes,
+    });
+  };
 
-        for (let i = 0; i < numberOfNodes; i++) {
-            let newNode = [
-                0,
-                i % elementsPerRow,
-                Math.floor(i / elementsPerRow),
-            ];
+  changeShowAlgorithm = () => {
+    this.setState({
+      showAlgorithm: false,
+    });
+  };
 
-            this.setState((prevState) => ({
-                nodes: [...prevState.nodes, newNode],
-            }));
-        }
-    };
+  setStart = (position) => {
+    // the node triggers this event when the 's' key is pressed over it
+    // prevents another start from being created
+    this.setState({
+      startExists: true,
+      startPos: position,
+    });
+  };
 
-    changeNodeStatus = (status, position) => {
-        // when a Node is clicked, it will trigger this method
-        // (change its status in this.state.nodes)
-        let updatedNodes = [...this.state.nodes];
-        let index = this.state.elementsPerRow * position[1] + position[0];
-        updatedNodes[index][0] = status;
-        this.setState({
-            nodes: updatedNodes,
-        });
-    };
+  setEnd = (position) => {
+    // the node triggers this event when the 'e' key is pressed over it
+    // prevents another end from being created
+    this.setState({
+      endExists: true,
+      endPos: position,
+    });
+  };
 
-    changeShowAlgorithm = () => {
-        this.setState({
-            showAlgorithm: false,
-        });
-    };
-
-    setStart = (position) => {
-        // the node triggers this event when the 's' key is pressed over it
-        // prevents another start from being created
-        this.setState({
-            startExists: true,
-            startPos: position,
-        });
-    };
-
-    setEnd = (position) => {
-        // the node triggers this event when the 'e' key is pressed over it
-        // prevents another end from being created
-        this.setState({
-            endExists: true,
-            endPos: position,
-        });
-    };
-
-    runAlgorithm = (algorithm) => {
-        if (algorithm === "DFS") {
-            let algorithm = new DFS();
-            // run DFS if a starting position and ending position have been set
-            if (this.state.startPos === null || this.state.endPos === null) {
-                console.log("There is no starting or ending position set!");
-            } else {
-                // get the visited nodes from the DFS traversal
-                let pathVisitedNodes = algorithm.dfs(
-                    this.state.nodes,
-                    this.state.elementsPerRow,
-                    [2, ...this.state.startPos],
-                    [3, ...this.state.endPos]
-                );
-                const visitedNodes = pathVisitedNodes[0];
-                const pathNodes = pathVisitedNodes[1];
-                this.setState({
-                    visited: visitedNodes,
-                    path: pathNodes,
-                    showAlgorithm: true,
-                });
-            }
-            this.props.changeRun("DFS");
-        } else if (algorithm === "Dijkstra's") {
-            console.log("run Dijkstra's");
-            this.props.changeRun("Dijkstra's");
-        }
-    };
-
-    componentDidMount() {
-        // when the page loads, create an empty board
-        this.createEmptyBoard();
-    }
-
-    componentDidUpdate() {
-        // when the clear board button is clicked in the nav, all the settings are reset
-        if (this.props.reset) {
-            // clear the start and end nodes
-            this.setState({
-                startExists: false,
-                endExists: false,
-                startPos: null,
-                endPos: null,
-            });
-            //switch off the reset
-            this.props.changeReset();
-        }
-        if (this.props.resetStartEnd) {
-            this.setState({
-                startExists: false,
-                endExists: false,
-                startPos: null,
-                endPos: null,
-            });
-            // switch off the reset
-            this.props.changeStartEndReset();
-        }
-        if (this.props.run) {
-            this.runAlgorithm(this.props.algorithm);
-        }
-    }
-
-    render() {
-        return (
-            <div className="board">
-                {this.state.nodes.map((element) => (
-                    <Node
-                        key={[element[1], element[2]]}
-                        position={[element[1], element[2]]}
-                        status={element[0]}
-                        changeStatus={this.changeNodeStatus}
-                        reset={this.props.reset}
-                        resetStartEnd={this.props.resetStartEnd}
-                        startExists={this.state.startExists}
-                        endExists={this.state.endExists}
-                        setStart={this.setStart}
-                        setEnd={this.setEnd}
-                        changeShowAlgorithm={this.changeShowAlgorithm}
-                        showAlgorithm={this.state.showAlgorithm}
-                        visited={this.state.visited}
-                        path={this.state.path}
-                    ></Node>
-                ))}
-            </div>
+  runAlgorithm = (algorithm) => {
+    if (algorithm === "DFS") {
+      let algorithm = new DFS();
+      // run DFS if a starting position and ending position have been set
+      if (this.state.startPos === null || this.state.endPos === null) {
+        console.log("There is no starting or ending position set!");
+      } else {
+        // get the visited nodes from the DFS traversal
+        let pathVisitedNodes = algorithm.dfs(
+          this.state.nodes,
+          this.state.elementsPerRow,
+          [2, ...this.state.startPos],
+          [3, ...this.state.endPos]
         );
+        const visitedNodes = pathVisitedNodes[0];
+        const pathNodes = pathVisitedNodes[1];
+        this.setState({
+          visited: visitedNodes,
+          path: pathNodes,
+          showAlgorithm: true,
+        });
+      }
+      this.props.changeRun("DFS");
+    } else if (algorithm === "Dijkstra's") {
+      console.log("run Dijkstra's");
+      this.props.changeRun("Dijkstra's");
     }
+  };
+
+  componentDidMount() {
+    // when the page loads, create an empty board
+    this.createEmptyBoard();
+  }
+
+  componentDidUpdate() {
+    // when the clear board button is clicked in the nav, all the settings are reset
+    if (this.props.reset) {
+      // clear the start and end nodes
+      this.setState({
+        startExists: false,
+        endExists: false,
+        startPos: null,
+        endPos: null,
+      });
+      //switch off the reset
+      this.props.changeReset();
+    }
+    if (this.props.resetStartEnd) {
+      this.setState({
+        startExists: false,
+        endExists: false,
+        startPos: null,
+        endPos: null,
+      });
+      // switch off the reset
+      this.props.changeStartEndReset();
+    }
+    if (this.props.run) {
+      this.runAlgorithm(this.props.algorithm);
+    }
+  }
+
+  render() {
+    return (
+      <div className="board">
+        {this.state.nodes.map((element) => (
+          <Node
+            key={[element[1], element[2]]}
+            position={[element[1], element[2]]}
+            status={element[0]}
+            changeStatus={this.changeNodeStatus}
+            reset={this.props.reset}
+            resetStartEnd={this.props.resetStartEnd}
+            startExists={this.state.startExists}
+            endExists={this.state.endExists}
+            setStart={this.setStart}
+            setEnd={this.setEnd}
+            changeShowAlgorithm={this.changeShowAlgorithm}
+            showAlgorithm={this.state.showAlgorithm}
+            visited={this.state.visited}
+            path={this.state.path}
+            mazeSize={this.state.elementsPerRow}
+          ></Node>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Board;
--- src/components/Node.js
+++ src/components/Node.js
@@ -1,124 +1,138 @@
 import React, { Component } from "react";
 
 class Node extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            position: this.props.position,
-            status: this.props.status,
-        };
+    this.state = {
+      position: this.props.position,
+      status: this.props.status,
+    };
+  }
+
+  // change from a wall to passage and vice-versa when a node is clicked
+  // unless designated as a starting node or ending node.
+  changeWallPassage = () => {
+    if (this.state.status === 1 || this.state.status === 0) {
+      this.setState((prevState) => ({
+        status: (prevState.status + 1) % 2,
+      }));
+
+      this.props.changeStatus((this.state.status + 1) % 2, this.props.position);
+    }
+  };
+
+  createStartEnd = (event) => {
+    // only create the start/end if the node is not already a wall
+    // can only create one start and one ending point on the board
+    if (this.state.status === 0) {
+      if (event.keyCode === 69 && !this.props.endExists) {
+        // create an end node at this postion if 'e' is pressed
+        // and a end doesn't exist
+        this.setState({
+          status: 3,
+        });
+        this.props.changeStatus(3, this.props.position);
+        this.props.setEnd(this.props.position);
+      } else if (event.keyCode === 83 && !this.props.startExists) {
+        // create a start node at this position if 's' is pressed
+        // and an start doesn't exist
+        this.setState({
+          status: 2,
+        });
+        this.props.changeStatus(2, this.props.position);
+        this.props.setStart(this.props.position);
+      }
+    }
+  };
+
+  mouseEnter = () => {
+    document.addEventListener("keydown", this.createStartEnd);
+  };
+  mouseLeave = () => {
+    document.removeEventListener("keydown", this.createStartEnd);
+  };
+
+  createBorderWall = () => {
+    if (
+      this.state.position[0] === 0 ||
+      this.state.position[0] === this.props.mazeSize - 1 ||
+      this.state.position[1] === 0 ||
+      this.state.position[1] === this.props.mazeSize - 1
+    ) {
+      this.setState({
+        status: 1,
+      });
+      this.props.changeStatus(1, this.state.position);
+    }
+  };
+
+  componentDidUpdate() {
+    if (this.props.reset) {
+      this.setState({
+        status: 0,
+      });
+      this.props.changeStatus(0, this.state.position);
+
+      // then create a border wall around the board
+      this.createBorderWall();
     }
 
-    // change from a wall to passage and vice-versa when a node is clicked
-    // unless designated as a starting node or ending node.
-    changeWallPassage = () => {
-        if (this.state.status === 1 || this.state.status === 0) {
-            this.setState((prevState) => ({
-                status: (prevState.status + 1) % 2,
-            }));
-
-            this.props.changeStatus(
-                (this.state.status + 1) % 2,
-                this.props.position
-            );
-        }
-    };
-
-    createStartEnd = (event) => {
-        // only create the start/end if the node is not already a wall
-        // can only create one start and one ending point on the board
-        if (this.state.status === 0) {
-            if (event.keyCode === 69 && !this.props.endExists) {
-                // create an end node at this postion if 'e' is pressed
-                // and a end doesn't exist
-                this.setState({
-                    status: 3,
-                });
-                this.props.changeStatus(3, this.props.position);
-                this.props.setEnd(this.props.position);
-            } else if (event.keyCode === 83 && !this.props.startExists) {
-                // create a start node at this position if 's' is pressed
-                // and an start doesn't exist
-                this.setState({
-                    status: 2,
-                });
-                this.props.changeStatus(2, this.props.position);
-                this.props.setStart(this.props.position);
-            }
-        }
-    };
-
-    mouseEnter = () => {
-        document.addEventListener("keydown", this.createStartEnd);
-    };
-    mouseLeave = () => {
-        document.removeEventListener("keydown", this.createStartEnd);
-    };
-
-    componentDidUpdate() {
-        if (this.props.reset) {
-            this.setState({
-                status: 0,
-            });
-            this.props.changeStatus(0, this.state.position);
-        }
-
-        if (this.props.resetStartEnd) {
-            if (
-                this.state.status === 2 ||
-                this.state.status === 3 ||
-                this.state.status === 4 ||
-                this.state.status === 5
-            ) {
-                this.setState({
-                    status: 0,
-                });
-                this.props.changeStatus(0, this.state.position);
-            }
-        }
-        if (this.props.showAlgorithm) {
-            // go through each visited node, and update it's state to 4 (visited)
-            this.props.visited.forEach((node) => {
-                // check if the position of the node is the same as any of the elements in the list
-                if (
-                    this.state.position[0] === node[1] &&
-                    this.state.position[1] === node[2]
-                ) {
-                    // if the node is found, update the state of the node to visited (4)
-                    this.setState({
-                        status: 4,
-                    });
-                    this.props.changeStatus(4, this.props.position);
-                }
-            });
-            this.props.path.forEach((node) => {
-                if (
-                    this.state.position[0] === node[1] &&
-                    this.state.position[1] === node[2]
-                ) {
-                    // if the node is found, update the state of the node to visited (4)
-                    this.setState({
-                        status: 5,
-                    });
-                    this.props.changeStatus(4, this.props.position);
-                }
-            });
-            this.props.changeShowAlgorithm();
-        }
+    if (this.props.resetStartEnd) {
+      if (
+        this.state.status === 2 ||
+        this.state.status === 3 ||
+        this.state.status === 4 ||
+        this.state.status === 5
+      ) {
+        this.setState({
+          status: 0,
+        });
+        this.props.changeStatus(0, this.state.position);
+      }
     }
-
-    render() {
-        return (
-            <div
-                className={`status-${this.state.status}`}
-                id={this.state.position}
-                onClick={this.changeWallPassage}
-                onMouseEnter={this.mouseEnter}
-                onMouseLeave={this.mouseLeave}
-            ></div>
-        );
+    if (this.props.showAlgorithm) {
+      // go through each visited node, and update it's state to 4 (visited)
+      this.props.visited.forEach((node) => {
+        // check if the position of the node is the same as any of the elements in the list
+        if (
+          this.state.position[0] === node[1] &&
+          this.state.position[1] === node[2]
+        ) {
+          // if the node is found, update the state of the node to visited (4)
+          this.setState({
+            status: 4,
+          });
+          this.props.changeStatus(4, this.props.position);
+        }
+      });
+      this.props.path.forEach((node) => {
+        if (
+          this.state.position[0] === node[1] &&
+          this.state.position[1] === node[2]
+        ) {
+          // if the node is found, update the state of the node to visited (4)
+          this.setState({
+            status: 5,
+          });
+          this.props.changeStatus(4, this.props.position);
+        }
+      });
+      this.props.changeShowAlgorithm();
     }
+  }
+
+  render() {
+    return (
+      <div
+        className={`status-${this.state.status}`}
+        id={this.state.position}
+        onClick={this.changeWallPassage}
+        onMouseEnter={this.mouseEnter}
+        onMouseLeave={this.mouseLeave}
+      ></div>
+    );
+  }
 }
 
 export default Node;
